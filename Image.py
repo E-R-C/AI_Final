4571a42ae37b1290e31a11ad2b41e7e3ce958d97
E-R-C <ehuynh.school@gmailcom>
2017-12-05 23:50:09 -0600

    Creates a small som map

--- Image.py
+++ Image.py
@@ -1,19 +1,27 @@
 import random
-
-
+import numpy as np
+import cv2
 class Image:
     # Default 1024x768
     def __init__(self, width=1024, height=768,two_d_array=[]):
         if len(two_d_array) >= 1:
+            # print(two_d_array.shape)
             self.width = len(two_d_array[0])
             self.height = len(two_d_array)
-            # print("width: " + str(self.width))
-            # print("Height: " + str(self.height))
-            self.array = two_d_array
+            # There might be an issue here... come back if you have time
+            # self.array = [[[0,0,0] for _ in range(height)] for _ in
+            #  range(width)]
+            # for w in range(width):
+            #     for h in range(height):
+            #         self.array[w][h] = two_d_array[h][w]
+            # print(np.transpose(two_d_array, (1,0,2)).shape)
+            self.array = np.transpose(two_d_array, (1,0,2))
+
+
         else:
             self.width = width
             self.height = height
-            self.array = [[[random.randint(0,256),random.randint(0,256),random.randint(0,256)] for _ in range(width)] for _ in range(height)]
+            self.array = [[[random.randint(0,255),random.randint(0,255),random.randint(0,255)] for _ in range(height)] for _ in range(width)]
 
     def calculate_dist(self, image2):
         sum = 0
@@ -25,13 +33,13 @@ class Image:
                     sum += abs(pixel[i] -  pixel2[i])
         return sum
     def pixel_at(self,w, h):
-        return self.array[h][w]
+        return self.array[w][h]
     def set_pixel(self,w,h,triple_array):
-        self.array[h][w] = triple_array
+        self.array[w][h] = triple_array
     def apply_image(self,image2, weight):
         for w in range(self.width):
             for h in range(self.height):
                 triple_array = self.pixel_at(w,h)
                 triple_array2 = image2.pixel_at(w,h)
                 for i in range(len(triple_array)):
-                    triple_array[i] = triple_array[i] * (1 - weight) + triple_array2[i] * weight
+                    self.array[w][h][i] = triple_array[i] * (1 - weight) + triple_array2[i] * weight
