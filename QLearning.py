0f9e8036b42c0111b57edd224656cf5293d26293
annaholmes <holmesaa@hendrix.edu>
2017-12-06 17:08:03 -0600

    q_learning!

--- QLearning.py
+++ QLearning.py
@@ -2,32 +2,35 @@ import random
 import gym
 import universe
 
-
+from SOM import SOM
+import cv2
+import numpy as np
+import scipy.misc
 
 '''def determine_action(observation_n, reward, data):
-    # determine state using SOM using observation_n
-    # determine reward of current state
-    som_state = 0 # get int state by passing observation_n to SOM
-    # if is new state, append new state to qTable w/ actions
-    # SOM shouldn't be out of bounds
-    if som_state > len(data.qTable):
-        data.qTable.append([0.0, 0.0, 0.0])
-    max_reward = max(data.qTable[som_state])
-    index_of_reward = data.qTable[som_state].index(max_reward)
+	# determine state using SOM using observation_n
+	# determine reward of current state
+	som_state = 0 # get int state by passing observation_n to SOM
+	# if is new state, append new state to qTable w/ actions
+	# SOM shouldn't be out of bounds
+	if som_state > len(data.qTable):
+		data.qTable.append([0.0, 0.0, 0.0])
+	max_reward = max(data.qTable[som_state])
+	index_of_reward = data.qTable[som_state].index(max_reward)
 
-    if random.uniform(0.0, 1.0) < data.e:
-        action = random.choice(data.action_list)
-    else:
-        action = data.action_list[index_of_reward]
+	if random.uniform(0.0, 1.0) < data.e:
+		action = random.choice(data.action_list)
+	else:
+		action = data.action_list[index_of_reward]
 
-    return action'''
-
-
+	return action'''
 
 
 
-    # at the next step forward, do q_update
-    # env.step() ? to return
+
+
+	# at the next step forward, do q_update
+	# env.step() ? to return
 
 
 
@@ -35,98 +38,158 @@ import universe
 
 
 '''def q_update(next_state, reward, data): # int, int
-    # inc timestep
-    data.timestep += 1
-    next_reward = max(data.qTable[next_state])
-    new_value = (1 - data.alpha) * data.qTable[data.state][data.action] + data.alpha * (data.beta * next_reward + reward)
-    data.state = next_state
-    data.alpha = 1.0 / (1 + (data.timestep / 200.0)) #arbitrary constant
-    e = 1.0 / (1 + (data.timestep / 80.0))
-    return new_value'''
+	# inc timestep
+	data.timestep += 1
+	next_reward = max(data.qTable[next_state])
+	new_value = (1 - data.alpha) * data.qTable[data.state][data.action] + data.alpha * (data.beta * next_reward + reward)
+	data.state = next_state
+	data.alpha = 1.0 / (1 + (data.timestep / 200.0)) #arbitrary constant
+	e = 1.0 / (1 + (data.timestep / 80.0))
+	return new_value'''
+
+from universe import wrappers
+def crop_photo(array,top_left_x, top_left_y, bottom_right_x, bottom_right_y):
+	return array[top_left_y:bottom_right_y,top_left_x:bottom_right_x]
+
+
+#def qUpdate(current_state, prev_state)
+
+def fill_contour(orig):
+	img = orig.copy()
+	img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	img2 = cv2.medianBlur(img2, 5)
+	ret, img2 = cv2.threshold(img2, 60, 255, cv2.THRESH_BINARY)
+
+	# des = cv2.bitwise_not(img2)
+	ret, contour, hier = cv2.findContours(img2, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
+	for cnt in contour:
+		cv2.drawContours(img2, [cnt], 0, 255, -1)
+	cv2.imshow("contours", img2)
+	return img2
 
 
 def main():
-    env = gym.make('internet.SlitherIO-v0')
-    observation_n = env.reset()
-    qTable = [[0.0 for i in range(4)] for j in range(10)]
+	env = gym.make('internet.SlitherIO-v0')
+	env.configure(remotes=1)
+	observation_n = env.reset()
+	qTable = [[0.0 for i in range(4)] for j in range(100)]
+	som_h = 10
+	som_w = 10
+	Som = SOM(height=som_h, width=som_w, image_height= 90, image_width=150, radius=3)  # 10 state SOM
+	#SomGrd = [[0.0 for j in range(som_h)] for i in range(som_w)]
+	#data = Data(SOM, qTable)
+	state = 0  # might have to turn into int
+	reward = 0.0
+	alpha = 0.0
+	beta = 0.0
+	action_i = 0
+	prev_action_i = 0
+	timestep = 0
+	temp_timestep = 0
+	e = 1.0 # prob of taking random action
+	left = [('KeyEvent', 'ArrowUp', False), ('KeyEvent', 'ArrowLeft', True), ('KeyEvent', 'ArrowRight', False)]
+	right = [('KeyEvent', 'ArrowUp', False), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', True)]
+	forward = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', False)]
+	still = [('KeyEvent', 'ArrowUp', False), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', False)]
 
-    SOM = None  # 10 state SOM
-
-    #data = Data(SOM, qTable)
-    state = 0  # might have to turn into int
-    reward = 0.0
-    alpha = 0.0
-    beta = 0.0
-    action_i = 0
-    prev_action_i = 0
-    timestep = 0
-    temp_timestep = 0
-    e = 1.0 # prob of taking random action
-    left = [('KeyEvent', 'ArrowUp', False), ('KeyEvent', 'ArrowLeft', True), ('KeyEvent', 'ArrowRight', False)]
-    right = [('KeyEvent', 'ArrowUp', False), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', True)]
-    forward = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', False)]
-    still = [('KeyEvent', 'ArrowUp', False), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', False)]
-
-    action_list = [right, left, forward, still]
+	action_list = [right, left, forward, still]
+	reward_track = np.array([0, 0, 0, 0, 0, 0])
 
 
-    # qTable organized by [state : [action, action], state : [action, action]]
+	# qTable organized by [state : [action, action], state : [action, action]]
+	#print(env2.reset())
+
+	action_i = random.randint(0, len(action_list) - 1)
+
+	# determine next action
+	while True:
+		timestep += 1
+		if timestep > 1:
+			temp_timestep += 1
+			if temp_timestep > 5:
+				temp_timestep = 0
+				if observation_n[0] != None:
+
+					action_n = [action_list[action_i] for ob in observation_n]
+					prev_action_i = action_i
+
+					# take step w/ action
+					# universe.rewarder.remote? universe
+					# update q-table with next_state &
+					shrunken_image = fix_image(observation_n[0]["vision"])
+					(x, y) = Som.train(shrunken_image)  # get int state by passing observation_n to SOM
+					next_state = y * som_w + x
+					# if is new state, append new state to qTable w/ actions
+					# SOM shouldn't be out of bounds
+					'''if som_state > len(data.qTable):
+						data.qTable.append([0.0, 0.0, 0.0])'''
+					max_reward = max(qTable[next_state]) + reward_track.sum()
+					for index in range(len(reward_track), 0, -1):
+						reward_track[index] = reward_track[index - 1]
+					reward_track[-1] = reward_n[0]
+					index_of_reward = qTable[next_state].index(max_reward)
+					if random.uniform(0.0, 1.0) < e:
+						action_i = random.randint(0, len(action_list) - 1)
+					else:
+						action_i = index_of_reward
+				else:
+					action_n = [action_list[action_i] for ob in observation_n]
+			else:
+				action_n = [still for ob in observation_n]
+
+			observation_n, reward_n, done_n, info = env.step(action_n)
+			if (info['n'][0]['stats.reward.count'] != 0):
+				print(info['n'][0]['stats.reward.count'])
+
+			reward = reward_n[0]
+
+			#next_state = observation_n[0] # get next state from SOM
+			if observation_n[0] != None:
+				#update q table
+				shrunken_image = fix_image(observation_n[0]["vision"])
+				(x, y) = Som.train(shrunken_image)
+				next_state = y * som_w + x
+
+				timestep += 1
+				next_reward = max(qTable[next_state])
+				new_value = (1 - alpha) * qTable[state][prev_action_i] + alpha * (beta * next_reward + reward)
+				alpha = 1.0 / (1 + (timestep / 200.0))  # arbitrary constant
+				e = 1.0 / (1 + (timestep / 2000.0))
+
+				qTable[state][prev_action_i] = new_value
+				state = next_state
 
 
-    action_i = random.randint(0, len(action_list) - 1)
+			env.render()
 
-    # determine next action
-    while True:
-        timestep += 1
 
-        if timestep > 1:
-            temp_timestep += 1
-            if temp_timestep > 5:
-                temp_timestep = 0
-                if observation_n[0] != None:
 
-                    action_n = [action_list[action_i] for ob in observation_n]
-                    prev_action_i = action_i
 
-                    # take step w/ action
-                    # universe.rewarder.remote? universe
-                    print("keys: " + universe.rewarder["reward"])
-                    # update q-table with next_state &
 
-                    som_state = random.randrange(0, 10)  # get int state by passing observation_n to SOM
-                    # if is new state, append new state to qTable w/ actions
-                    # SOM shouldn't be out of bounds
-                    '''if som_state > len(data.qTable):
-                        data.qTable.append([0.0, 0.0, 0.0])'''
-                    max_reward = max(qTable[som_state])
-                    index_of_reward = qTable[som_state].index(max_reward)
-                    if random.uniform(0.0, 1.0) < e:
-                        action_i = random.randint(0, len(action_list) - 1)
-                    else:
-                        action_i = index_of_reward
-                else:
-                    action_n = [action_list[action_i] for ob in observation_n]
-            else:
-                action_n = [still for ob in observation_n]
 
-            observation_n, reward_n, done_n, info = env.step(action_n)
-            reward = reward_n[0]
-            #next_state = observation_n[0] # get next state from SOM
-            next_state = random.randrange(0,10)
-            env.render()
 
-            #update q_table
-            timestep += 1
-            next_reward = max(qTable[next_state])
-            new_value = (1 - alpha) * qTable[state][prev_action_i] + alpha * (beta * next_reward + reward)
-            alpha = 1.0 / (1 + (timestep / 200.0))  # arbitrary constant
-            e = 1.0 / (1 + (timestep / 2000.0))
 
-            qTable[state][prev_action_i] = new_value
 
+
+
+
+def fix_image(img):
+	top_left_x = 20
+	top_left_y = 85
+	bottom_right_x = 520
+	bottom_right_y = 385
+	photo_array = crop_photo(img, top_left_x, top_left_y, bottom_right_x,
+							 bottom_right_y)
+	shrunken_image = scipy.misc.imresize(photo_array, 0.3, "nearest")
+	cvImage = cv2.cvtColor(shrunken_image, cv2.COLOR_RGB2BGR)
+
+	cv2.imshow("RobotVision", shrunken_image)
+	cv2.waitKey(1)
+	return shrunken_image
 
 if __name__ == '__main__':
-    main()
+	main()
 
 
 
---
+++ Q_Learning2.0.py
@@ -0,0 +1,175 @@
+from SOM import SOM
+import cv2
+import numpy as np
+import gym
+import universe
+
+import math
+import scipy.misc
+import random
+# track reward probably
+
+class QLearner():
+
+    def __init__(self, width, height, action_list, random_threshold=1.0, exploration_threshold=20, decay_rate=.95, learning_rate_constant=10.0):
+        self.width = width
+        self.height = height
+        self.action_list = action_list
+
+        self.random_threshold = random_threshold
+        self.exploration_threshold = exploration_threshold
+
+        self.learning_rate_constant = learning_rate_constant
+        self.decay_rate = decay_rate
+        self.q_table = [[[0.0 for q in range(len(action_list))] for i in range(height)] for j in range(width)]
+        self.learning_rate = 1.0
+        self.discount_factor = .5
+        self.times_chosen = [[[0 for q in range(len(action_list))] for i in range(height)] for j in range(width)]
+
+
+    def select_action(self, x, y):
+        saved_index = 0
+        max_reward = 0
+
+        # exploration
+        if random.random() < self.random_threshold:
+            return random.choice(self.action_list)
+
+        for times_chosen in range(len(self.times_chosen[x][y])):
+            if self.times_chosen[x][y][times_chosen] < self.exploration_threshold:
+                return self.action_list[times_chosen]
+
+        # exploitation
+        for action_reward_index in range(len(self.q_table[x][y])):
+            if self.q_table[x][y][action_reward_index] > max_reward:
+                max_reward = self.q_table[x][y][action_reward_index]
+                saved_index = action_reward_index
+
+
+        return self.action_list[saved_index]
+
+
+    def update_qtable(self, prev_state_w, prev_state_h, state_w, state_h, prev_action, prev_reward):
+        action_i = self.action_list.index(prev_action)
+        learning_rate = 1.0 / (1 + (self.times_chosen[prev_state_w][prev_state_h][action_i]) / self.learning_rate_constant) # higher the constant, higher the learning rate
+        self.q_table[prev_state_w][prev_state_h][action_i] = (1 - learning_rate) * self.q_table[prev_state_w][prev_state_h] + learning_rate * (self.discount_factor *
+                                                                                                                                               self.max_reward(state_w, state_h) + prev_reward)
+        self.random_threshold *= self.decay_rate
+
+    def max_reward(self, state_w, state_h):
+        return max(self.q_table[state_w][state_h])
+
+
+
+
+def main():
+    # setup
+    env = gym.make('internet.SlitherIO-v0')
+    env.configure(remotes=1)
+    observation_n = env.reset()
+
+
+    tiny_image_h = 12
+    tiny_image_w = 18  # for tiny image
+    SOM_WIDTH = 6
+    SOM_HEIGHT = 6
+    som = SOM.SOM(SOM_WIDTH, SOM_HEIGHT, tiny_image_w, tiny_image_h, radius=3)
+
+    ## Define Actions on keyboard
+    possible_actions = []
+    left = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', True), ('KeyEvent', 'ArrowRight', False)]
+    right = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', True)]
+    forward = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', False)]
+    still = [('KeyEvent', 'ArrowUp', False), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', False)]
+    possible_actions.append(left)
+    possible_actions.append(right)
+    possible_actions.append(forward)
+    possible_actions.append(still)
+    default_action_n = [still for ob in observation_n]
+
+    q_learner = QLearner(SOM_WIDTH, SOM_HEIGHT, possible_actions)
+
+    frame_buffer = []
+
+
+    action = still
+    prev_action = still
+    state = 0
+    prev_state = 0
+
+    while True:
+
+        if observation_n[0] != None:
+
+            frame_buffer.append(observation_n[0]["vision"])
+            if len(frame_buffer) > 10:
+                frame_buffer.pop(0)
+
+            action_n = [action for ob in observation_n]
+            image_for_som = process_image(observation_n[0]["vision"], tiny_image_w, tiny_image_h)
+            state_x, state_y = som.train(image_for_som)
+            action = q_learner.select_action(state_x, state_y)
+            q_learner.update_qtable(prev_action, action, prev_state, state, reward_n[0])
+            prev_action = action
+            prev_state = state
+            print('reward: ' + reward_n[0])
+
+        else:
+            # do stuff with death here
+            action_n = [default_action_n for ob in observation_n]
+
+        observation_n, reward_n, done_n, info = env.step(action_n)
+        env.render()
+
+def process_image(observation, tiny_image_h, tiny_image_w):
+    top_left_x = 20
+    top_left_y = 85
+    bottom_right_x = 520
+    bottom_right_y = 385
+    photo_array = crop_photo(observation, top_left_x, top_left_y, bottom_right_x,
+                             bottom_right_y)
+    shrunken_image = scipy.misc.imresize(photo_array, 0.5, "nearest")
+    shrunken_image = cv2.cvtColor(shrunken_image, cv2.COLOR_RGB2BGR)
+    shrunken_image2 = cv2.cvtColor(shrunken_image, cv2.COLOR_BGR2GRAY)
+
+    tiny = cv2.resize(fill_contour(shrunken_image2), (tiny_image_w, tiny_image_h))
+    return tiny
+
+def crop_photo(array,top_left_x, top_left_y, bottom_right_x, bottom_right_y):
+    return array[top_left_y:bottom_right_y,top_left_x:bottom_right_x]
+
+def fill_contour(orig):
+    img = orig.copy()
+    img = cv2.medianBlur(img, 5)
+    ret, img = cv2.threshold(img, 60, 255, cv2.THRESH_BINARY)
+    ret, contour, hier = cv2.findContours(img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contour:
+        cv2.drawContours(img, [cnt], 0, 255, -1)
+
+    detector = cv2.MSER_create()
+    fs = detector.detect(img)
+    fs.sort(key=lambda x: -x.size)
+    sfs = [x for x in fs if not supress(x, fs)]
+    h, w = orig.shape
+    final_img = np.zeros((h, w, 3), np.uint8)
+
+    for f in sfs:
+        cv2.circle(final_img, (int(f.pt[0]), int(f.pt[1])), int(f.size / 2), (0, 255, 0), cv2.FILLED)
+    #cv2.imshow("binary", img2)
+    #cv2.imshow("contours", final_img)
+    return final_img
+
+def supress(x, fs):
+    # leaderboard
+    if (x.pt[0] > 165.0 and x.pt[0] < 208.0 and x.pt[1] < 15.0):
+        return True
+    # mini map
+    if (x.pt[0] > 195.0 and x.pt[0] < 230.0 and x.pt[1] < 120.0 and x.pt[1] > 98.0):
+        return True
+    for f in fs:
+        distx = f.pt[0] - x.pt[0]
+        disty = f.pt[1] - x.pt[1]
+        dist = math.sqrt(distx * distx + disty * disty)
+        if (f.size > x.size) and (dist < f.size / 2):
+            return True
